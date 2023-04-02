96d5b0424651bfe4a852a3c9568a1a5c0997385d
Petr Kulhanek <kulhanek@chemi.muni.cz>
2023-04-02 12:06:36 +0200

    keys print

--- scripts/create-fig-pae.py
+++ scripts/create-fig-pae.py
@@ -104,7 +104,9 @@ if not args.input.exists():
 
 print("\nInput model: {0}".format(args.input))
 model_dict = pickle.load(open(args.input,'rb'))
-print("Model keys:  " + str(model_dict.keys()))
+print("Model keys:\n")
+for key in model_dict.keys():
+    print("  * %s\n" % key)
 
 if not ('predicted_aligned_error' in model_dict):
     print('\nNo predicted_aligned_error present in data!\n')
